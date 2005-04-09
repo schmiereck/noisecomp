c76fe227449673e7495184e93f55fe2cf209e973
smk23 <>
2005-04-09 09:01:07 +0000

    *** empty log message ***

---
+++ NoiseComp/src/de/schmiereck/noiseComp/desktopPage/widgets/TrackData.java
@@ -0,0 +1,15 @@
+package de.schmiereck.noiseComp.desktopPage.widgets;
+/*
+ * Created on 09.04.2005, Copyright (c) schmiereck, 2005
+ */
+/**
+ * <p>
+ * 	TODO docu type
+ * </p>
+ * 
+ * @author smk
+ * @version <p>09.04.2005:	created, smk</p>
+ */
+public class TrackData
+{
+}
