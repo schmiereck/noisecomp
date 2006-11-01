7afb8bc52078e8200f3e8ec15808cbdbd61b6766
smk23 <>
2006-11-01 02:06:19 +0000

    Herauslösen des View.

---
+++ NoiseComp/src/de/schmiereck/noiseComp/view/desktopController/editModulPage/actions/CancelGroupButtonActionLogicListener.java
@@ -0,0 +1,40 @@
+package de.schmiereck.noiseComp.view.desktopController.editModulPage.actions;
+
+import de.schmiereck.noiseComp.view.desctopPage.widgets.ButtonActionLogicListenerInterface;
+import de.schmiereck.noiseComp.view.desctopPage.widgets.InputWidgetData;
+import de.schmiereck.noiseComp.view.desktopController.DesktopControllerData;
+import de.schmiereck.noiseComp.view.desktopController.DesktopControllerLogic;
+
+/**
+ * TODO docu
+ *
+ * @author smk
+ * @version <p>22.02.2004: created, smk</p>
+ */
+public class CancelGroupButtonActionLogicListener
+	implements ButtonActionLogicListenerInterface
+{
+	private DesktopControllerLogic controllerLogic;
+	private DesktopControllerData controllerData;
+	
+	/**
+	 * Constructor.
+	 * 
+	 * 
+	 */
+	public CancelGroupButtonActionLogicListener(DesktopControllerLogic controllerLogic, DesktopControllerData controllerData)
+	{
+		super();
+		
+		this.controllerLogic = controllerLogic;
+		this.controllerData = controllerData;
+	}
+
+	/* (non-Javadoc)
+	 * @see de.schmiereck.noiseComp.desktopPage.widgets.ButtonActionLogicListenerInterface#notifyButtonReleased(de.schmiereck.noiseComp.desktopPage.widgets.InputWidgetData)
+	 */
+	public void notifyButtonReleased(InputWidgetData buttonData)
+	{
+		this.controllerData.setActiveDesktopPageData(this.controllerData.getMainDesktopPageData());
+	}
+}
