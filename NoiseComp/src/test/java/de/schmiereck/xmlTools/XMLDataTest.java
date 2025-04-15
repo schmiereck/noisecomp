03b3b02c3c1f683262b104427337c38db92e8caf
Thomas Schmiereck <thomas@schmiereck.de>
2025-04-15 11:04:52 +0200

    Open in Windsurf.

--- NoiseComp/src/test/java/de/schmiereck/xmlTools/XMLDataTest.java
+++ NoiseComp/src/test/java/de/schmiereck/xmlTools/XMLDataTest.java
@@ -6,7 +6,6 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.w3c.dom.traversal.NodeIterator;
 
 import junit.framework.TestCase;
 
